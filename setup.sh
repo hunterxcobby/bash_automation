2ea0e394a1a1f482ef6a314896611ecb28457985
hunterxcobby <solomonsefah13@gmail.com>
2023-11-25 15:22:00 +0000

    add

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Hello! This script is automatically executed after cloning."
-# Add more setup instructions as needed
+echo "Hey Babe, \
+Grant me the permission?:)*"
 
+while true; do
+    sleep 0.5
+    echo "Waiting for your sudo password..."
+    sudo apt-get install -y toilet && break
+done
+
+ # Display in a different font and colors
+toilet -f bigmono9 -F gay "Happy Birthday!"

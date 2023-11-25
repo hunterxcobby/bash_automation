3bbbd72c55ee6bcf61c0f080b775e9266b095627
hunterxcobby <solomonsefah13@gmail.com>
2023-11-25 15:38:51 +0000

    pushed

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Hey Babe, \
-Grant me the permission?:)*"
+Grant me the permission to hack your heart?:)*"
 
 while true; do
-    sleep 0.5
-    echo "Waiting for your sudo password..."
+    sleep 1.5
+    echo "Do not worry, this will only take some time"
+    sleep 1.5
+    echo "Accessing your database..."
+    sleep 2.0
     sudo apt-get install -y toilet && break
 done
 
  # Display in a different font and colors
-toilet -f bigmono9 -F gay "Happy Birthday!"
+toilet -f term -F border --gay "Happy Birthday!"
